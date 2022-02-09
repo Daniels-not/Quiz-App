c43ed8ea36b5bddbe6aed8fc771a7b45bc3c172f
Ramy Daniel Campusano Volquez <ramydcampusano@hotmail.com>
2022-02-09 16:51:02 -0400

    quiz app completed

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,47 @@
+const startBtn = document.getElementById("start_game");
+const information = document.getElementById("card_information");
+const question_container = document.getElementById("question_container");
+
+let shuffleQuestions, currentQuestion;
+const questionText = document.getElementById("question");
+const answerBtn = document.getElementById("answer_buttons");
+
+const correctOrIncorrect = document.getElementById("question_checker");
+const allDone = document.getElementById("all_done");
+const highScore = document.getElementById("high_score");
+
+const score = document.getElementById("score");
+const goBack = document.getElementById("go_back");
+const clearHighScore = document.getElementById("clear_high_score");
+
+const line = document.getElementsByClassName("line");
+const submitScore = document.getElementById("submit_score");
+const initials = document.getElementById("initials");
+
+const score_list = document.getElementById("score_list");
+const countDown = document.getElementById("timer");
+
+let history = [];
+
 const questions = [
   {
     questionText: "Commonly used data types DO NOT include:",
     options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
     answer: "3. alerts",
+    correctAnswer: (index) => {
+      if (index === 2) {
+        correctOrIncorrect.innerText = "Correct!";
+        setTimeout(() => {
+          nextQuestion();
+        } , 400);
+      } else {
+        correctOrIncorrect.innerText = "Incorrect!";
+        setTimeout(() => {
+          nextQuestion();
+          timeLeft-=10;
+        } , 400);
+      }
+    }
   },
   {
     questionText: "Arrays in JavaScript can be used to store ______.",
@@ -13,12 +52,40 @@ const questions = [
       "4. all of the above",
     ],
     answer: "4. all of the above",
+    correctAnswer: (index) => {
+      if (index === 3) {
+        correctOrIncorrect.innerText = "Correct!";
+        setTimeout(() => {
+          nextQuestion();
+        } , 400);
+      } else {
+        correctOrIncorrect.innerText = "Incorrect!";
+        setTimeout(() => {
+          nextQuestion();
+          timeLeft-=10;
+        } , 400);
+      }
+    }
   },
   {
     questionText:
       "String values must be enclosed within _____ when being assigned to variables.",
     options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
     answer: "3. quotes",
+    correctAnswer: (index) => {
+      if (index === 2) {
+        correctOrIncorrect.innerText = "Correct!";
+        setTimeout(() => {
+          nextQuestion();
+        } , 400);
+      } else {
+        correctOrIncorrect.innerText = "Incorrect!";
+        setTimeout(() => {
+          nextQuestion();
+          timeLeft-=10;
+        } , 400);
+      }
+    }
   },
   {
     questionText:
@@ -30,82 +97,148 @@ const questions = [
       "4. console.log",
     ],
     answer: "4. console.log",
+    correctAnswer: (index) => {
+      if (index === 3) {
+        correctOrIncorrect.innerText = "Correct!";
+        setTimeout(() => {
+          nextQuestion();
+        } , 400);
+      } else {
+        correctOrIncorrect.innerText = "Incorrect!";
+        setTimeout(() => {
+          nextQuestion();
+          timeLeft-=10;
+        } , 400);
+      }
+    }
   },
   {
     questionText:
       "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
     options: ["1. break", "2. stop", "3. halt", "4. exit"],
     answer: "1. break",
+    correctAnswer: (index) => {
+      if (index === 0) {
+        correctOrIncorrect.innerText = "Correct!";
+        setTimeout(() => {
+          nextQuestion();
+        } , 400);
+      } else {
+        correctOrIncorrect.innerText = "Incorrect!";
+        setTimeout(() => {
+          nextQuestion();
+          timeLeft-=10;
+        } , 400);
+      }
+    }
   },
 ];
 
-
-const startBtn = document.getElementById("start-game");
-const information = document.getElementById("card-information");
-const question_container = document.getElementById("question-container");
-
-let shuffleQuestions, currentQuestion;
-const questionText = document.getElementById("question");
-const answerBtn = document.getElementById("answer-buttons");
-
-const correctOrIncorrect = document.getElementById("question-checker");
-
-const startGame = () => {
-  console.log("start game");
-  information.classList.add("hide");
-  question_container.classList.remove("hide");
-
-  shuffleQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestion = 0;
-
-  setNextQuestion();
-};
-
-const resetState = () => {
-  setTimeout(() => {
-    correctOrIncorrect.innerText = "";
-    while (answerBtn.firstChild) { // remove all children from answerBtn div (answer buttons)
-      answerBtn.removeChild(answerBtn.firstChild);
-    }
-    setNextQuestion();
-  }, 1000);
-};
-
-const setNextQuestion = () => {
-  resetState(); // reset state of the questions and answers
-  showQuestion(); // show question and options from array of questions and options
-  currentQuestion++;
-};
-
-const showQuestion = () => {
-  questionText.innerHTML = shuffleQuestions[currentQuestion].questionText; // set question text from array of questions
-
-  shuffleQuestions[currentQuestion].options.forEach((option) => {
-    const button = document.createElement("button");
-    button.innerText = option;
-    button.classList.add("btn");
-    answerBtn.appendChild(button);
-  }) // set options from array of questions and options
-};
-
-const setStatusClass = (element, correct) => {
-  clearStatusClass(element);
-  if (correct === element.dataset.answer) {
-    correctOrIncorrect.innerText = "Correct!";
-  }
-  else {
-    correctOrIncorrect.innerText = "Incorrect!";
+function generateQuestion(index = 0) { // index is the current question
+  for(i = 0; i <= 3 ; i++) {
+    answerBtn.innerHTML += `
+      <button class="btn" onClick="questions[${index}].correctAnswer(${i})">${questions[index].options[i]}</button>
+    `;
   }
 }
 
-const selectAnswer = () => {
-  const correctAnswer = shuffleQuestions[currentQuestion].answer;
-  setStatusClass(document.body, correctAnswer);
-  Array.from(answerBtn.children).forEach((button) => {
-    setStatusClass(button, button.dataset.answer);
-  });
+function clearDom() {
+  questionText.innerHTML = "";
+  answerBtn.innerHTML = "";
+  correctOrIncorrect.innerHTML = "";
+}
+
+// create a count down timer
+
+let timeLeft = 60;
+
+const timer = () => {
+  let timerInterval = setInterval(function() {
+    if(timeLeft > 0) {
+      timeLeft--;
+      countDown.innerText = timeLeft;
+    } else {
+      clearInterval(timerInterval);
+      question_container.classList.add("hide");
+      allDone.classList.remove("hide");
+    }
+  } , 1000);
+}
+
+
+// Start Game
+
+function startGame(){
+  clearDom();
+  information.classList.add("hide");
+  question_container.classList.remove("hide");
+  currentQuestion = 0;
+
+  shuffleQuestions = questions.sort(() => Math.random() - 0.5);
+  questionText.innerText = questions[currentQuestion].questionText;
+
+  generateQuestion(currentQuestion);
+  timer();
 };
+
+function nextQuestion() { // next question function to be called when the answer is correct or incorrect
+  clearDom();
+  if(currentQuestion < questions.length - 1) {
+    currentQuestion++;
+    questionText.innerText = questions[currentQuestion].questionText;
+    generateQuestion(currentQuestion);
+  }else{
+    question_container.classList.add("hide");
+    allDone.classList.remove("hide");
+    countDown.innerText = "Test Complete!";
+  }
+}
+
+function generateScoreList() { // generate the score list when the game is over and the user wants to see the score list
+  score_list.innerHTML = "";
+  for(i = 0; i < history.length; i++) {
+    score_list.innerHTML += `
+      <ol>
+        <h2 class="subScore">
+          <li>
+            ${history[i].initials} - ${history[i].score}
+          </li>
+        </h2>
+      </ol>
+    `;
+  }
+}
+
+const emptyHightScore = () => history.length = 0; // empty the history array when the game is over
 
 startBtn.addEventListener("click", startGame);
 
+submitScore.addEventListener("click", (e) => {
+  e.preventDefault();
+  history.push({
+    score: timeLeft,
+    initials: initials.value
+  });
+  localStorage.setItem("highScore", JSON.stringify(history));
+  history = JSON.parse(localStorage.getItem("highScore")); // if there is a high score in local storage, then set the high score to the high score in local storage
+  timeLeft = 0;
+  allDone.classList.add("hide");
+  highScore.classList.remove("hide");
+  generateScoreList();
 
+});
+
+clearHighScore.addEventListener("click", () => {
+  highScore.classList.add("hide");
+  allDone.classList.add("hide");
+  information.classList.remove("hide");
+  localStorage.clear();
+  emptyHightScore();
+});
+
+goBack.addEventListener("click", () => {
+  highScore.classList.add("hide");
+  allDone.classList.remove("hide");
+});
+
+console.log(history);
